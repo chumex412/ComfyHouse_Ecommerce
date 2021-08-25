06ce6178fd86d3369566740ac340ab9eec141c26
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-08-25 05:51:07 +0100

    Added a component to store data in local storage

---
+++ src/Storage.js
@@ -0,0 +1,18 @@
+class Storage {
+
+  static saveProducts(products) {
+    localStorage.setItem('products', JSON.stringify(products));
+  }
+
+  static getProduct(id) {
+    const products = JSON.parse(localStorage.getItem("products"));
+  
+    return products.find(product => product.id === id);
+  }
+
+  static saveCart(cart) {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }
+}
+
+export default Storage;
