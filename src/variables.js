2a2c25e3fb753c94183fffe1505eaa11bd24e594
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-08-26 11:58:25 +0100

    Created varaibles files

---
+++ src/variables.js
@@ -0,0 +1,10 @@
+// UI Elements 
+export const cartOverlay = document.querySelector('.cart-overlay'),
+      cartDOM = document.querySelector('.cart'),
+      closeBtn = document.querySelector('.close-cart'),
+      cartTotal = document.querySelector('.cart-total'),
+      clearCartBtn = document.querySelector('.clear-cart'),
+      productDOM = document.querySelector('.product-content'),
+      cartBtn = document.querySelector('.cart-btn'),
+      cartList = document.querySelector('.cart-list'),
+      cartItems = document.querySelector('.cart-items');
