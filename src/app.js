8642851d483074b37034601e9d048645b6e8b17e
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-08-26 12:00:32 +0100

    Moved the UI variables to variables file

--- src/app.js
+++ src/app.js
@@ -2,20 +2,6 @@ import Products from "./Product";
 import UI from "./UI";
 import Storage from "./Storage";
 
-// UI Elements 
-const cartOverlay = document.querySelector('.cart-overlay'),
-      cartDOM = document.querySelector('.cart'),
-      closeBtn = document.querySelector('.close-cart'),
-      cartTotal = document.querySelector('.cart-total'),
-      clearCart = document.querySelector('.clear-cart'),
-      productDOM = document.querySelector('.product-content'),
-      cartBtn = document.querySelector('.cart-btn'),
-      cartList = document.querySelector('.cart-list'),
-      cartItems = document.querySelector('.cart-items');
-
-let cart = [];
-let buttonDOM;
-
 
 // Render immediately after browser loads
 document.addEventListener('DOMContentLoaded', () => {
@@ -26,20 +12,15 @@ document.addEventListener('DOMContentLoaded', () => {
   products.getProducts()
     .then(product => {
       // Display products in the UI
-      ui.displayProducts(productDOM, product);
+      ui.displayProducts(product);
+      
+      ui.setupAPP();
 
       // Set products to local storage
       Storage.saveProducts(product);
     })
     .then( () => {
-      ui.getBagButtons(
-        cart, 
-        buttonDOM, 
-        cartTotal, 
-        cartItems, 
-        cartList,
-        cartDOM,
-        cartOverlay
-      );
+      ui.getBagButtons();
+      ui.cartLogic();
     })
 });
