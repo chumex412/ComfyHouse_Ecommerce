a0cb08c0f06218521f931e5a393e783076da806d
chumex412 <64347090+chumex412@users.noreply.github.com>
2021-08-26 12:02:16 +0100

    Added get cart function

--- src/Storage.js
+++ src/Storage.js
@@ -13,6 +13,10 @@ class Storage {
   static saveCart(cart) {
     localStorage.setItem('cart', JSON.stringify(cart));
   }
+
+  static getCart() {
+    return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
+  }
 }
 
 export default Storage;
